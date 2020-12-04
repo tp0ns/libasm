215b5549426337e600e7828dc22a985d640646b8
tp0ns <tpons@student.42.fr>
2020-12-04 12:43:27 +0100

    Explanation ft_strlen

--- ft_strlen.s
+++ ft_strlen.s
@@ -7,8 +7,8 @@ ft_strlen:
 	jmp		count;					;Program counter point now to count function
 
 count:								;count function
-	cmp		BYTE [rdi + rax], 0;	;compare the value of the byte pointed to by "rdi + rax" to 0 (actually does 0 - [rdi + rax]) if == 0 then zeroflag == 1 
-	je		stop					;if zero flag == 1 then program counter point to stop function
+	cmp		BYTE [rdi + rax], 0;	;compare the value of the byte pointed to by "rdi + rax" to 0 (actually does 0 - [rdi + rax] without modyfing anything) if == 0 then zeroflag == 1 
+	je		stop					;if zero flag == 1 then program counter point to stop function (je(jump equal) is alias for jz(jump if zero)
 	inc		rax						;increment the value stored in rax
 	jmp		count					;program counter point again to count function
 
